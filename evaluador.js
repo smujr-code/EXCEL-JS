999bd50321b9db2d776d749f95d5bd50c65faaeb
smujr-code <smujr@miumg.edu.gt>
2026-09-10 23:37:59 -0600

    agregué el recorrido para separar y clasificar los tokens de la fórmula

--- evaluador.js
+++ evaluador.js
@@ -1,6 +1,6 @@
-// Analiza una porción de texto individual y determina si corresponde
-//  a una función estadística (SUMA, PROMEDIO, MAX, MIN), una referencia
-//  de celda válida (REF), un número flotante (NUM) o un error léxico (ERROR).
+/* Analiza una porción de texto individual y determina si corresponde
+a una función estadística (SUMA, PROMEDIO, MAX, MIN), una referencia
+ de celda válida (REF), un número flotante (NUM) o un error léxico (ERROR).*/
 
 function clasificarToken(t) {
     let tUpper = t.toUpperCase();
@@ -18,4 +18,38 @@ function clasificarToken(t) {
 
         return { tipo: "NUM", valor: numero };
     }
+}
+
+// Recorre la fórmula carácter por carácter para separar operadores y separadores, y agrupa los textos para su posterior clasificación.
+
+function tokenizarFormula(formula) {
+    let resultadoTokens = [];
+    let token = "";
+
+    for (let i = 0; i < formula.length; i++) {
+        let caracter = formula[i];
+
+        if ("+-*/(),:;".includes(caracter)) {
+            if (token !== "") {
+                resultadoTokens.push(clasificarToken(token.trim()));
+                token = "";
+            }
+
+            if (caracter !== "," && caracter !== ";") {
+                resultadoTokens.push({ tipo: "OP", valor: caracter });
+            } else {
+                resultadoTokens.push({ tipo: "SEPARADOR", valor: caracter });
+            }
+        } else {
+            if (caracter !== "=" && caracter !== " ") {
+                token += caracter;
+            }
+        }
+    }
+
+    if (token !== "") {
+        resultadoTokens.push(clasificarToken(token.trim()));
+    }
+
+    return resultadoTokens;
 }
