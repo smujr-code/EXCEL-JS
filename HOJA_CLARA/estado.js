83b4bc3f2e662a7a54a6cdc70f7cc90245c48b43
smujr-code <smujr@miumg.edu.gt>
2026-09-10 22:18:08 -0600

    agregué la función para actualizar las celdas y conecté el estado EJ-9

--- HOJA_CLARA/estado.js
+++ HOJA_CLARA/estado.js
@@ -1,2 +1,14 @@
+/*Crea el objeto central en memoria que almacenará la
+información de las celdas de forma independiente al DOM*/
 JavaScript
 const estadoCeldas = {};
+
+/*Define la función que asigna o actualiza el valor de 
+una celda específica dentro del objeto de estado global*/
+function guardarValorCelda(ref, valor) {
+    if (!estadoCeldas[ref]) {
+        estadoCeldas[ref] = {};
+    }
+
+    estadoCeldas[ref].valor = valor;
+}
