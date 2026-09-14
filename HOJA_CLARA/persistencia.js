3d4a48cbe0341460a2022a30f3c759cc4355aa64
smujr-code <smujr@miumg.edu.gt>
2026-09-14 10:14:38 -0600

    botón cargar agregado con sus funciones correspondientes

--- HOJA_CLARA/persistencia.js
+++ HOJA_CLARA/persistencia.js
@@ -5,3 +5,17 @@ function guardarEstadoLocal() {
     localStorage.setItem("hojaClaraData", JSON.stringify(estadoCeldas));
     alert("¡Hoja guardada exitosamente en el almacenamiento local!");
 }
+// Carga los datos guardados en el almacenamiento local y actualiza el estado de las celdas 
+function cargarEstadoLocal() {
+    let datosGuardados = localStorage.getItem("hojaClaraData");
+    if (datosGuardados) {
+        let parsedData = JSON.parse(datosGuardados);
+
+        Object.keys(estadoCeldas).forEach(k => delete estadoCeldas[k]);
+        Object.assign(estadoCeldas, parsedData);
+
+        location.reload();
+    } else {
+        alert("No hay datos guardados previamente.");
+    }
+}
