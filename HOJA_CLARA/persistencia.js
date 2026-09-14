645ae0bc65a8aa1d8e04330f32c5c7c85ebf7ed3
smujr-code <smujr@miumg.edu.gt>
2026-09-14 12:44:45 -0600

    función de exportar incluida al presionar botón exportar

--- HOJA_CLARA/persistencia.js
+++ HOJA_CLARA/persistencia.js
@@ -19,11 +19,33 @@ function cargarEstadoLocal() {
         alert("No hay datos guardados previamente.");
     }
 }
-
+//limpia las hoja al borrar cual tipo de caracter que ella contenga.
 function limpiarTodaLaHoja() {
     if (confirm("¿Estás seguro de que deseas limpiar toda la hoja y borrar los datos guardados?")) {
         localStorage.removeItem("hojaClaraData");
         Object.keys(estadoCeldas).forEach(k => delete estadoCeldas[k]);
         location.reload();
     }
+}
+//permite la exportación de los datos contenidos en la hoja clara, guardandolos en descarga como hojaclara_exportado.csv
+function exportarACSV() {
+    let csvContent = "";
+    for (let i = 1; i <= TOTAL_FILAS; i++) {
+        let filaArr = [];
+        for (let j = 1; j <= TOTAL_COLUMNAS; j++) {
+            let ref = `${generarLetraColumna(j)}${i}`;
+            let val = obtenerValorCelda(ref) || "";
+            filaArr.push(`"${val}"`);
+        }
+        csvContent += filaArr.join(",") + "\n";
+    }
+
+    let blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    let url = URL.createObjectURL(blob);
+    let link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("download", "hojaclara_exportado.csv");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
